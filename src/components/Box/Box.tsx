60801a7b30de905de2f353f900eb65d5ba836419
Kaitlyn DiLorenzo <kdilorenzo2@gmail.com>
2021-04-22 16:57:34 -0700

    Small code improvements

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -4,7 +4,7 @@ import React, { ChangeEvent, FunctionComponent, useState } from 'react';
 import { Form } from 'react-bootstrap';
 
 const Box: FunctionComponent = () => {
-  const [inputValue, setInputValue] = useState(' ');
+  const [inputValue, setInputValue] = useState('');
 
   const validateInput = (e: ChangeEvent<HTMLInputElement>) => {
     const input = e.target.value;
@@ -14,13 +14,15 @@ const Box: FunctionComponent = () => {
         input.charCodeAt(0) > 48 &&
         input.charCodeAt(0) < 58)
     ) {
-      setInputValue(e.target.value);
+      setInputValue(input);
     } else if (
       input.length === 2 &&
       input.charCodeAt(1) > 48 &&
       input.charCodeAt(1) < 58
     ) {
-      setInputValue(input.charAt(1));
+      setInputValue(
+        input.charAt(0) === inputValue ? input.charAt(1) : input.charAt(0)
+      );
     }
   };
 
